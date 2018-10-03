7c9fd4afe6ebafbcf24aa2cd2e586eda7ecf6269
Abi SA <abisa3002@gmail.com>
2018-10-03 21:10:27 +0700

    Adding Adapter, Model, Network, and Activity Package

---
+++ app/src/main/java/com/abisayuti/info_ba/Activity/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.abisayuti.info_ba.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.abisayuti.info_ba.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
--- app/src/main/java/com/abisayuti/info_ba/Activity/MainActivity.java
+++ app/src/main/java/com/abisayuti/info_ba/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.abisayuti.info_ba;
+package com.abisayuti.info_ba.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.abisayuti.info_ba.R;
 
 public class MainActivity extends AppCompatActivity {
 
